1f83362c234c561159f113796cf8995bfdf26de3
Nathan Dai <nathandai2000@gmail.com>
2020-06-26 13:11:44 -0700

    move files

--- Data Collection/Meteorological/historical_weather.py
+++ Data Collection/Meteorological/historical_weather.py
@@ -2,12 +2,12 @@ from wwo_hist import retrieve_hist_data
 
 API_KEY = '62c4f496efb147c1b2160953202406'
 
-frequency = 24
+frequency = 24 # hours
 start_date = '01-JAN-2020'
 end_date = '22-JUN-2020'
 api_key = API_KEY
 county_list = ['Cook', 'Los Angeles', 'Queens', 'Kings', 'Bronx', 'Nassau', 'Suffolk', 'Westchester']
-location_list = ['60120','90012','11365','11217','10453','12123','23433','10502']
+location_list = ['60120','90012','11365','11217','10453','12123','23433','10502'] # zip code
 hist_weather_data = retrieve_hist_data(api_key,
                                 location_list,
                                 start_date,
